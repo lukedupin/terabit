a11de9538640faaf9c1247c5533127639482cde9
Luke Dupin <lukedupin@gmail.com>
2021-03-31 16:34:20 -0700

    Getting nft swipe hooked up

--- frontend/src/nft_collection.js
+++ frontend/src/nft_collection.js
@@ -7,18 +7,42 @@ export default class NftCollection extends React.Component {
         super(props);
         this.state = {
         };
+
+        this.handleBuy = this.handleBuy.bind(this);
+    }
+
+    handleBuy() {
     }
 
     render() {
         const { human, nfts } = this.props;
 
-        console.log(human.username);
-
         return (
-            <div className="nft-collection">
-                <img src={human.image_profile} />
-                <div>{human.username}</div>
-                {Object.entries(nfts).map(([k, nft]) => <NftCard nft={nft} />)}
+            <div className="row">
+                <div className="map-listing">
+                    <div className="map-listing-head">
+                        <img className="map-listing-profile" src="{human.profile_image}" alt="img"/>
+                        <div className="map-listing-profile-deets">
+                            <a className="user-name" href="#">{human.username}</a>
+                            <p className="user-byline">$0.00</p>
+                        </div>
+                        <div className="map-listing-cta">
+                            <a className="btn" href="#" onClick={this.handleBuy}>Buy</a>
+                        </div>
+                    </div>
+                    <div className="map-listing-item">
+                        <img className="map-listing-image" src="/static/images/image-location-default.jpg" alt="img"/>
+                        <p className="location-latlong">43.615021, -116.202316</p>
+                        <p className="location-story">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero sapien, mattis sed cursus non, sollicitudin vitae leo. Nullam auctor et dui sit amet ultricies. Nullam eget porttitor nisi, vel condimentum erat.</p>
+                        <p className="tokens-header">Tokens</p>
+                        <p className="token-title">Benny Wonderbear</p>
+                        <p className="token-desc">#1821767 Gen 8</p>
+                        <p className="token-title">Benny Wonderbear</p>
+                        <p className="token-desc">#1821767 Gen 8</p>
+                        <p className="token-title">Benny Wonderbear</p>
+                        <p className="token-desc">#1821767 Gen 8</p>
+                    </div>
+                </div>
             </div>
         );
     }
--- frontend/src/sidebar.js
+++ frontend/src/sidebar.js
@@ -31,60 +31,7 @@ export default class Sidebar extends React.Component {
         return (
             <div className="col-lg-3">
                 <div className="map-listings">
-
-                    <div className="row">
-                        <div className="map-listing">
-                            <div className="map-listing-head">
-                                <img className="map-listing-profile" src="/static/images/user-default.png" alt="img"/>
-                                <div className="map-listing-profile-deets">
-                                    <a className="user-name" href="#">Richard Cranium</a>
-                                    <p className="user-byline">$1,234</p>
-                                </div>
-                                <div className="map-listing-cta">
-                                    <a className="btn" href="#">Buy</a>
-                                </div>
-                            </div>
-                            <div className="map-listing-item">
-                                <img className="map-listing-image" src="/static/images/image-location-default.jpg" alt="img"/>
-                                <p className="location-latlong">43.615021, -116.202316</p>
-                                <p className="location-story">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero sapien, mattis sed cursus non, sollicitudin vitae leo. Nullam auctor et dui sit amet ultricies. Nullam eget porttitor nisi, vel condimentum erat.</p>
-                                <p className="tokens-header">Tokens</p>
-                                <p className="token-title">Benny Wonderbear</p>
-                                <p className="token-desc">#1821767 Gen 8</p>
-                                <p className="token-title">Benny Wonderbear</p>
-                                <p className="token-desc">#1821767 Gen 8</p>
-                                <p className="token-title">Benny Wonderbear</p>
-                                <p className="token-desc">#1821767 Gen 8</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="row">
-                        <div className="map-listing">
-                            <div className="map-listing-head">
-                                <img className="map-listing-profile" src="/static/images/user-default.png" alt="img"/>
-                                <div className="map-listing-profile-deets">
-                                    <a className="user-name" href="#">Richard Cranium</a>
-                                    <p className="user-byline">$1,234</p>
-                                </div>
-                                <div className="map-listing-cta">
-                                    <a className="btn" href="#">Buy</a>
-                                </div>
-                            </div>
-                            <div className="map-listing-item">
-                                <img className="map-listing-image" src="/static/images/image-location-default.jpg" alt="img"/>
-                                <p className="location-latlong">43.615021, -116.202316</p>
-                                <p className="location-story">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum libero sapien, mattis sed cursus non, sollicitudin vitae leo. Nullam auctor et dui sit amet ultricies. Nullam eget porttitor nisi, vel condimentum erat.</p>
-                                <p className="tokens-header">Tokens</p>
-                                <p className="token-title">Benny Wonderbear</p>
-                                <p className="token-desc">#1821767 Gen 8</p>
-                                <p className="token-title">Benny Wonderbear</p>
-                                <p className="token-desc">#1821767 Gen 8</p>
-                                <p className="token-title">Benny Wonderbear</p>
-                                <p className="token-desc">#1821767 Gen 8</p>
-                            </div>
-                        </div>
-                    </div>
+                    {Object.entries(humans).map(([k,v]) => { <NftCollection human={v} />})}
                 </div>
             </div>
         );
