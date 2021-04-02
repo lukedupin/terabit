9f19a108673b984db30e2db9ac278428084be9ed
Luke Dupin <lukedupin@gmail.com>
2021-04-02 10:57:46 -0700

    Meta mask start

--- frontend/src/pages/profile.js
+++ frontend/src/pages/profile.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Button } from "react-bootstrap";
 
 export default class Profile extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleConnectMetaMask = this.handleConnectMetaMask.bind(this)
+    }
+
+    async handleConnectMetaMask() {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        for ( let i = 0; i < accounts.length; i++ ) {
+            const account = accounts[i];
+            console.log(account)
+        }
     }
 
     render() {
+        if (typeof window.ethereum === 'undefined') {
+            return (<Redirect to="/" />)
+        }
+
         return (
             <div>
+                <Button onClick={this.handleConnectMetaMask}>Connect MetaMask</Button>
                 Profile
             </div>
         );
