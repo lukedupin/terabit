3fbb14861dab06c1c27c101af99ae19a43aee45c
Luke Dupin <lukedupin@gmail.com>
2021-04-02 10:05:39 -0700

    MetaMask initial detection setup

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Modal, Button} from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 import Main from '../pages/main';
@@ -13,11 +14,18 @@ export default class App extends React.Component {
         super(props);
 
         this.state = {
-            collapsed: true
+            collapsed: true,
+            show_get_started: false,
+            reload_required: false,
         };
 
         this.toggleNav = this.toggleNav.bind(this)
         this.collapseNav = this.collapseNav.bind(this)
+        this.handleCloseModal = this.handleCloseModal.bind(this)
+        this.handleOpenModal = this.handleOpenModal.bind(this)
+        this.handleGetMetaMask = this.handleGetMetaMask.bind(this)
+        this.handleReload = this.handleReload.bind(this)
+
     }
 
     collapseNav( collapsed ) {
@@ -33,9 +41,30 @@ export default class App extends React.Component {
         return (test == window.location.pathname)? "active": ""
     }
 
+    handleOpenModal() {
+        this.setState({show_get_started: true})
+    }
+
+    handleCloseModal() {
+        this.setState({show_get_started: false})
+    }
+
+    handleGetMetaMask() {
+        window.open("https://metamask.io/download", "_blank")
+        this.setState({reload_required: true})
+    }
+
+    handleReload() {
+        window.location.reload(true);
+        this.setState({show_get_started: false})
+    }
+
     render() {
+        const { show_get_started, reload_required } = this.state;
         //Is the nav collapsed?
         const collapsed_klass = (this.state.collapsed)? "collapse": "";
+
+        const has_meta_mask = (typeof window.ethereum !== 'undefined');
 
         return (
             <header className="site_header">
@@ -61,11 +90,30 @@ export default class App extends React.Component {
                         </ul>
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item pr-0" onClick={this.collapseNav}>
-                                <Link className="btn btn-ghost" to={'/profile'}>My Profile</Link>
+                                {(has_meta_mask)? <Link className="btn btn-ghost" to={'/profile'}>My Profile</Link>:
+                                                  <a className="btn btn-ghost" onClick={this.handleOpenModal}>Get started</a>}
                             </li>
                         </ul>
                     </div>
                 </nav>
+
+                { !has_meta_mask &&
+                    <Modal show={show_get_started} onHide={this.handleCloseModal}>
+                        <Modal.Header closeButton>
+                            <Modal.Title>Get Started</Modal.Title>
+                        </Modal.Header>
+                        { (!reload_required)? <Modal.Body>Yo, you need MetaMask to get started with this stuff</Modal.Body>:
+                                              <Modal.Body>Reloading the page is required for MetaMask changes to take effect.</Modal.Body>}
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={this.handleCloseModal}>
+                                Close
+                            </Button>
+                            { (!reload_required)? <Button variant="primary" onClick={this.handleGetMetaMask}>Get MetaMask</Button>:
+                                                  <Button variant="primary" onClick={this.handleReload}>Reload page</Button>}
+
+                        </Modal.Footer>
+                    </Modal>
+                }
             </header>
         );
     }
--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -3,9 +3,17 @@ import React from 'react';
 export default class Contact extends React.Component {
     constructor(props) {
         super(props);
+
+        if (typeof window.ethereum !== 'undefined') {
+            console.log('MetaMask is installed!');
+        }
+        else {
+            console.log("No meta mask here!")
+        }
     }
 
     render() {
+
         return (
             <div>
                 Contact page
