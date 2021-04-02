a92e09b0c3cd7b4f0422753e263ce1f708310511
Luke Dupin <lukedupin@gmail.com>
2021-04-01 17:21:54 -0700

    Fixed filter_by styles

--- frontend/src/components/filter_by.js
+++ frontend/src/components/filter_by.js
@@ -1,35 +1,5 @@
 import React from 'react';
 
-class Checkbox extends  React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            checked: true,
-            klass: "",
-            label: props.label,
-            tab_idx: props.tab_idx,
-        };
-
-        this.handleToggle = this.handleToggle.bind(this);
-    }
-
-    handleToggle() {
-        const checked = !this.state.checked
-        this.setState({ checked });
-        this.props.onChange( this.state.label, checked )
-    }
-
-    render() {
-        const { checked, klass, label, tab_idx } = this.state;
-        return (
-            <label className={klass} onClick={this.handleToggle}>{label}
-                <input type="checkbox" checked={checked} onChange={this.handleToggle} tabIndex={tab_idx}/>
-                <span className="checkmark"></span>
-            </label>
-        );
-    }
-}
-
 export default class FilterBy extends React.Component {
     constructor(props) {
         super(props);
@@ -38,30 +8,10 @@ export default class FilterBy extends React.Component {
             claimed: true,
             empty: true,
         };
-
-        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidUpdate() {
         this.props.onChange( this.state )
-    }
-
-    handleChange( label, checked ) {
-        switch (label) {
-            case "For sale":
-                this.setState({for_sale: checked})
-                break;
-
-            case "Claimed":
-                this.setState({claimed: checked})
-                break;
-
-            case "Empty":
-                this.setState({empty: checked})
-                break;
-
-            default: break
-        }
     }
 
     render() {
@@ -71,16 +21,28 @@ export default class FilterBy extends React.Component {
             <div class="map-grid-filter">
                 <ul>
                     <li>
-                        <Checkbox onChange={this.handleChange} klass="check_item check_item_1" label="For sale" tab_idx="0" />
+                        <label className="check_item check_item_1" onChange={() => this.setState({ for_sale: !for_sale })}>For sale
+                            <input type="checkbox" checked={for_sale} tabIndex="0"/>
+                            <span className="checkmark"></span>
+                        </label>
                     </li>
                     <li>
-                        <Checkbox onChange={this.handleChange} klass="check_item check_item_2" label="Claimed" tab_idx="1" />
+                        <label className="check_item check_item_2" onChange={() => this.setState({ claimed: !claimed })}>Claimed
+                            <input type="checkbox" checked={claimed} tabIndex="2"/>
+                            <span className="checkmark"></span>
+                        </label>
                     </li>
                     <li>
-                        <Checkbox onChange={this.handleChange} klass="check_item check_item_3" label="Empty" tab_idx="2" />
+                        <label className="check_item check_item_3" onChange={() => this.setState({ empty: !empty})}>Empty
+                            <input type="checkbox" checked={empty} tabIndex="3"/>
+                            <span className="checkmark"></span>
+                        </label>
                     </li>
                     <li>
-                        <Checkbox onChange={this.handleChange} klass="check_item check_item_4" label="A thing" tab_idx="3" />
+                        <label className="check_item check_item_4">A thing
+                            <input type="checkbox" tabIndex="4"/>
+                            <span className="checkmark"></span>
+                        </label>
                     </li>
                 </ul>
             </div>
