cd3fd13ccfc4f85c068fc1d58b4532a7dc505b29
Luke Dupin <lukedupin@gmail.com>
2021-04-05 22:29:56 -0700

    Reordered

--- frontend/src/components/nft_card.js
+++ frontend/src/components/nft_card.js
@@ -18,11 +18,11 @@ export default class NftCard extends React.Component {
                 <div className="map-listing-cta">
                     <a className="btn" target="_blank" rel="noopener noreferrer" href={nft.listing_url}>Buy</a>
                 </div>
-                <img className="map-listing-image" src={nft.img} alt="img"/>
                 <p className="location-latlong"></p>
-                <a className="link-color" target="_blank" rel="noopener noreferrer" href={nft.url}>Link</a>
-                <p className="location-story">{nft.desc}</p>
+                <img className="map-listing-image" src={nft.img} alt="img"/>
                 <p className="tokens-header">{nft.name}</p>
+                <p className="location-story">{nft.desc}</p>
+                <a className="link-color" target="_blank" rel="noopener noreferrer" href={nft.url}>Link</a>
                 <p className="token-desc"></p>
             </div>
         );
