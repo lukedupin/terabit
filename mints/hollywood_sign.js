3fb32d2d27e3be7fe72bd991752ef04dba7a705c
Luke Dupin <lukedupin@gmail.com>
2021-04-06 22:32:10 -0700

    Adding hollywood_sign

---
+++ mints/hollywood_sign.js
@@ -0,0 +1,12 @@
+{
+    "username": "TeraMint",
+    "state_name": "California",
+    "mode": "grid",
+    "name": "Hollywood Sign",
+    "lat": 34.134052,
+    "lng": -118.321568,
+    "check_water": false,
+    "row": 1,
+    "col": 1,
+    "skip": []
+}
