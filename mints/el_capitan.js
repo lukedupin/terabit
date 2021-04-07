244c3912c63928641ba3435bec853ed10c1ae89a
Luke Dupin <lukedupin@gmail.com>
2021-04-06 22:36:41 -0700

    Adding el cap

---
+++ mints/el_capitan.js
@@ -0,0 +1,12 @@
+{
+    "username": "TeraMint",
+    "state_name": "California",
+    "mode": "grid",
+    "name": "El Capitan",
+    "lat": 37.732963,
+    "lng": -119.637946,
+    "check_water": false,
+    "row": 1,
+    "col": 1,
+    "skip": []
+}
